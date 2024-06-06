d69443c5664bad3c6a6a7483cc4cad0698d990d8
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-06-06 10:59:14 -0600

    writing file to system succeeded

--- src/app/api/createlisting/route.js
+++ src/app/api/createlisting/route.js
@@ -1,7 +1,29 @@
-import { writefile } from "fs/promises";
+import { writeFile } from "fs/promises";
 export async function POST(req, res) {
-  console.log("posted!");
   const formData = await req.formData();
   console.log(formData.get("image"));
+  const listingImage = formData.get("image");
+  console.log(listingImage.type);
+  const contentType = listingImage.type;
+  const mimeType = contentType.split("/")[1];
+  const listingImageBuffer = await listingImage.arrayBuffer();
+
+  await writeFile(
+    `image.${mimeType}`,
+    Buffer.from(listingImageBuffer),
+    (err) => {
+      if (err) {
+        console.error("Error saving image: ", err);
+      } else {
+        console.log("Image saved successfully");
+      }
+    },
+  );
+
+  //try {
+  //  await writeFile("image.png", base64ListingImage);
+  //} catch (e) {
+  //  console.error(e);
+  //}
   return new Response(200);
 }
